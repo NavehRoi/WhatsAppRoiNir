fdd957a7512082df0c463a309d3139dfc7713eea
Nir Nussbaum <140694045+NirNussbaum08@users.noreply.github.com>
2024-06-20 16:13:28 +0300

    Create initOurWhatsApp.sh

---
+++ initOurWhatsApp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install git -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo npm install -g pm2
+git clone git@github.com:NavehRoi/WhatsAppRoiNir.git
+cd parkingLot
+npm install
+node server.js
+echo "Deployment completed successfully. Application is running on port 3000."
